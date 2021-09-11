62effe0c64295395f11d7f61541e28a3d663ec86
Arthur Mendes Pereira <61280645+ArthurAMP@users.noreply.github.com>
2021-09-11 14:43:00 -0300

    feat: tts and translate

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request
 import requests
-from google.cloud import speech
-
-
+from google.cloud import speech, texttospeech_v1, texttospeech, translate_v2
+import os 
+import json
+import ast
 app = Flask(__name__)
 
 @app.route('/')
@@ -19,6 +20,7 @@ def search(item):
 @app.route('/audio', methods=['POST'])
 def stt():
     client = speech.SpeechClient()
+    client1 = texttospeech_v1.TextToSpeechClient()
     file1 = request.data
     audio = speech.RecognitionAudio(content=file1)
 
@@ -29,26 +31,40 @@ def stt():
     )
     response = client.recognize(config=config, audio=audio)
     stringzona = str([result.alternatives[0].transcript for result in response.results])
-    payload = {
-        "voice": {
-            "ssmlGender": "NEUTRAL",
-            "languageCode": "pt-BR",
-            "name": "lol"
-        },
-        "audioConfig": {
-            "audioEncoding": "OGG_OPUS",
-            "pitch": 0
-        },
-        "input": {
-            "text": "compra tv com vapi"
-        }
-    }
-    a = requests.post('https://texttospeech.googleapis.com/v1/text:synthesize?key=[INSERT_KEY_HERE]', data=payload)
-    data2 = a.text
+
     if "televisão" in stringzona.lower():
         r = requests.get('http://localhost:5000/search/monitor')
         data = r.text
-    return data2
+        data = data.replace("[", "")
+        data = data.replace("]", "")
+        data2 = ast.literal_eval(data)
+        data2 = data2["title"]
+    data2 = str(data2)
+    target = 'pt'
+    client2 = translate_v2.Client()
+    data2 = client2.translate(
+        data2,
+        target_language=target
+    )
+    ttstext = str(data2["translatedText"])
+    synthesis_input = texttospeech_v1.SynthesisInput(text=ttstext)
+    voice = texttospeech_v1.VoiceSelectionParams(
+        language_code='pt-BR',
+        ssml_gender=texttospeech_v1.SsmlVoiceGender.FEMALE
+    )
+    audio_config = texttospeech_v1.AudioConfig(
+        audio_encoding=texttospeech_v1.AudioEncoding.OGG_OPUS
+    )
+    response1 = client1.synthesize_speech(
+        input=synthesis_input,
+        voice=voice,
+        audio_config=audio_config
+    )
+    
+    with open('audio file1.ogg', 'wb') as output1:
+        output1.write(response1.audio_content)
+    
+    return str(data2["translatedText"])
     return str([result.alternatives[0].transcript for result in response.results])
     #return str(file1)
 
